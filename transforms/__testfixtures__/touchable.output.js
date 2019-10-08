c319a08419dc32a33b3116d3504edec45c5184a0
Tyree6 <54169094+Tyree6@users.noreply.github.com>
2019-10-08 10:34:57 -0500

    Update.touch.output.js

--- transforms/__testfixtures__/touchable.output.js
+++ transforms/__testfixtures__/touchable.output.js
@@ -1,25 +1,25 @@
 <TouchableBounce onPress={function() {}} style={{color: 'red'}}></TouchableBounce>;
 
 <View>
-  <TouchableBounce style={{color: 'red'}}>
+  <TouchableBounce style={{color: 'green'}}>
     <Image />
     <Image />
   </TouchableBounce>
 </View>;
 
 <TouchableBounce>
-  <CustomView style={{color: 'red'}}>
+  <CustomView style={{color: 'green'}}>
     <Image />
   </CustomView>
 </TouchableBounce>;
 
 <TouchableBounce>
-  <View>1</View>
-  <View>2</View>
+  <View>3</View>
+  <View>13</View>
 </TouchableBounce>;
 
 <TouchableOpacity onPress={function() {}} style={{color: 'red'}}></TouchableOpacity>;
 
 <TouchableHighlight onPress={function() {}}>
-  <View style={{color: 'red'}} />
+  <View style={{color: 'green'}} />
 </TouchableHighlight>;
